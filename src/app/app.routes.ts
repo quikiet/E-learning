623d4d0991742b8bd661648ef9b9a1f374fd3286
quikiet <quikiet03@gmail.com>
2025-05-14 17:20:15 +0700

    Add Login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './pages/User/main-layout/main-layout.component';
 import { HomeComponent } from './pages/User/home/home.component';
+import { LoginComponent } from './pages/both/login/login.component';
 
 export const routes: Routes = [
     {
         path: '', component: MainLayoutComponent,
         children: [
             { path: '', component: HomeComponent },
+            { path: 'login', component: LoginComponent },
         ]
     },
 ];
--- src/app/pages/both/login/login.component.ts
+++ src/app/pages/both/login/login.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  tab: string = 'login';
 
 }
