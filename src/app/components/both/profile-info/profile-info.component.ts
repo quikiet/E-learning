6d03e7087b0eb1eb661f68f92013c038dda294b1
quikiet <quikiet03@gmail.com>
2025-05-14 17:35:08 +0700

    Add Profile page

--- src/app/components/both/profile-info/profile-info.component.ts
+++ src/app/components/both/profile-info/profile-info.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar'; import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms'; import { Select } from 'primeng/select';
+import { DividerModule } from 'primeng/divider';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { RippleModule } from 'primeng/ripple';
+import { Skeleton } from 'primeng/skeleton';
+interface City {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-profile-info',
-  imports: [],
+  imports: [RippleModule, Select, AvatarModule, ButtonModule, InputTextModule, FormsModule, DividerModule, RouterOutlet, RouterLink, Skeleton],
   templateUrl: './profile-info.component.html',
   styleUrl: './profile-info.component.css'
 })
-export class ProfileInfoComponent {
+export class ProfileInfoComponent implements OnInit {
+  cities: City[] | undefined;
 
+  selectedCity: City | undefined;
+
+  ngOnInit() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 }
+
