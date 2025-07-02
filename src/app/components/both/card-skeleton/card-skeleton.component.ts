6dcd658449203fbb751c1abfc68a078d99b95eec
quikiet <quikiet03@gmail.com>
2025-07-02 13:38:02 +0700

    skeleton + loading

---
+++ src/app/components/both/card-skeleton/card-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Skeleton } from 'primeng/skeleton';
+@Component({
+  selector: 'app-card-skeleton',
+  imports: [Skeleton],
+  templateUrl: './card-skeleton.component.html',
+  styleUrl: './card-skeleton.component.css'
+})
+export class CardSkeletonComponent {
+
+}
